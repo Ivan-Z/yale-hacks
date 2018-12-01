9b990c5485f3886009b99a4cdc4281b6ba35f642
Ivan Zdane <izdane@gmail.com>
2018-12-01 11:50:52 -0500

    Added react navigation bar

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { FlatList } from 'react-native';
+import { Text, Button, Card, Divider } from 'react-native-elements';
+
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+
 
 // Import getNews function from news.js
 import { getNews } from './src/components/new.js';
 // We'll get to this one later
 import Article from './src/components/Article.js';
 
-export default class App extends React.Component {
+class HomeScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = { articles: [], refreshing: true };
@@ -44,4 +48,36 @@ export default class App extends React.Component {
   );
   }
 }
+
+class PoliticScreen extends React.Component {
+  render() {
+    return (
+      <Text> Hello Yale!</Text>
+  );
+  }
+}
+
+class TechScreen extends React.Component {
+  render() {
+    return (
+      <Text> Hello Yale!</Text>
+  );
+  }
+}
+class SportScreen extends React.Component {
+  render() {
+    return (
+      <Text> Hello Yale!</Text>
+  );
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  Home: HomeScreen,
+  Politics: PoliticScreen,
+  Tech: TechScreen,
+  Sports: SportScreen,
+});
+
+export default createAppContainer(TabNavigator);
 
