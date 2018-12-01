d18901c741c65ab21b359a8830c7563261b8333e
Ivan Zdane <izdane@gmail.com>
2018-12-01 00:58:47 -0500

    basic news app functionality :=

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList } from 'react-native';
+
+// Import getNews function from news.js
+import { getNews } from './src/components/new.js';
+// We'll get to this one later
+import Article from './src/components/Article.js';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { articles: [], refreshing: true };
+    this.fetchNews = this.fetchNews.bind(this);
+  }
+  // Called after a component is mounted
+  componentDidMount() {
+    this.fetchNews();
+   }
+
+  fetchNews() {
+    getNews()
+      .then(articles => this.setState({ articles, refreshing: false }))
+      .catch(() => this.setState({ refreshing: false }));
+  }
+
+  handleRefresh() {
+    this.setState(
+      {
+        refreshing: true
+    },
+      () => this.fetchNews()
+    );
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello Yale Hacks!</Text>
-      </View>
-    );
+      <FlatList
+        data={this.state.articles}
+        renderItem={({ item }) => <Article article={item} />}
+        keyExtractor={item => item.url}
+        refreshing={this.state.refreshing}
+        onRefresh={this.handleRefresh.bind(this)}
+      />
+  );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
